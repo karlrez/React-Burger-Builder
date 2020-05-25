08a5ac9bd4d7c3c0a46882cabe16aeb387a82516
Karl <karlrez123@gmail.com>
2020-05-25 01:18:23 -0400

    Updates

---
+++ precache-manifest.03b50947fed30f46b31e50961bfdb3bd.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6b677d97778a4d5a0bd31b8821feb5b9",
+    "url": "/React-BurgerBuilder/index.html"
+  },
+  {
+    "revision": "3c93b869ac25d90b7e55",
+    "url": "/React-BurgerBuilder/static/css/main.14a93892.chunk.css"
+  },
+  {
+    "revision": "8d71ba182a0674f4eea4",
+    "url": "/React-BurgerBuilder/static/js/2.0d0747a8.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/React-BurgerBuilder/static/js/2.0d0747a8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3c93b869ac25d90b7e55",
+    "url": "/React-BurgerBuilder/static/js/main.4879c157.chunk.js"
+  },
+  {
+    "revision": "8d9a8cc08f2ef85173a4",
+    "url": "/React-BurgerBuilder/static/js/runtime-main.99260994.js"
+  },
+  {
+    "revision": "b8503d262bffbfb7c67fd6762963e7d1",
+    "url": "/React-BurgerBuilder/static/media/burger.b8503d26.png"
+  }
+]);
